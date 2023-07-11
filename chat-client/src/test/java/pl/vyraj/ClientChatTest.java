05ad89229a093d2771e7180c642dea48d77dd689
Geralt Son <geraltson@o2.pl>
2023-07-11 10:59:48 +0200

    First version of final project for review

---
+++ chat-client/src/test/java/pl/vyraj/ClientChatTest.java
@@ -0,0 +1,20 @@
+package pl.vyraj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ClientChatTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ chat-server/src/test/java/pl/vyraj/ServerChatTest.java
@@ -0,0 +1,20 @@
+package pl.vyraj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ServerChatTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
